bf7d1acff7c99d7a4b72983b66ed4556f87d5c6e
mattyX20 <altobelli.matteo45@gmail.com>
2016-12-06 17:16:01 +0100

    test
    sono prove

---
+++ test/scripts/myjs.js
@@ -0,0 +1,15 @@
+"ciao questa è una prova"
+var occhiali = 12;
+var ottici = 50;
+var lenti = 20;
+console.log(occhiali);
+console.log(lenti);
+console.log(ottici);
+console.log(window);
+console.clear(window);
+window.alert("EH! Volevi")
+var totale = occhiali + ottici + lenti;
+console.log(totale);
+var ciao = "Ti Piacerebbe"
+console.log(ciao)
+console.log(ciao.length)
