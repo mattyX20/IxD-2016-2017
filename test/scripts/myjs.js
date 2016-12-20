7af0aff6f4310305ee952e71a04b820105803616
Matteo Altobelli <altobelli.matteo45@gmail.com>
2016-12-20 18:03:11 +0100

    V3
    Js Prompt

--- test/scripts/myjs.js
+++ test/scripts/myjs.js
@@ -13,125 +13,104 @@
 // var ciao = "Ti Piacerebbe"
 // console.log(ciao)
 // console.log(ciao.length)
-
 // var number = 1;
-
 // while (number <= 5) {
 // console.log(number);
 // number++
 // }
 // var treno = 1;
 // var totale = 12;
-
 // while (treno <= totale){
-
 //	console.log("il treno " + treno + " sta circolando");
 //	treno++;
 // }
-
 //var treno = 1;
 //var operativi = 6;
-
 //for ( treno; treno <= operativi; treno++){
-
-	//console.log("il treno " + treno +" e in funzione")
+//console.log("il treno " + treno +" e in funzione")
 //};
-
 //var treno = 1;
 //var nonoperativi = 6;
-
-
 //for ( treno; treno <= nonoperativi; treno++){
-
 //	console.log("il treno " + treno +" e fermo")
 //};
-
 //var operativi = 6;
 //var nonoperativi = 6;
 //var totale = nonoperativi + operativi;
 //console.log("i treni sono " + totale);
-
 // versione docente
 //var trenitoali = 12;
 //var operativi = 6;
 //var treno = 1;
-
 //while (treno <= operativi){
-
 //	console.log("il treno " + treno + " sta funzionando")
 //	treno++
 //}
 //for (var fermo = operativi +1; fermo <= trenitoali; fermo++){
-
 //	console.log("il treno" + fermo + " e in stazione")
 //}
 
 // versione 1
-var trenitoali = 12;
+/*var trenitoali = 12;
 var operativi = 6;
 var treno = 1;
-
-if (treno < operativi){
-	console.log("trenitalia funziona")
+if (treno < operativi) {
+    console.log("trenitalia funziona")
 }
-
 else {
-	console.log("trenitalia e in sciopero")
+    console.log("trenitalia e in sciopero")
 };
-
-if (treno > operativi){
-	console.log("trenitalia funziona")
+if (treno > operativi) {
+    console.log("trenitalia funziona")
 }
-
 else {
-	console.log("trenitalia e in sciopero")
+    console.log("trenitalia e in sciopero")
 };
-
-
 // versione 2
-
 var trenitoali = 12;
 var operativi = 6;
 var treno = 1;
-
-for (treno; treno <= trenitoali; treno++){
-
-	if (treno <= operativi){
-
-		console.log("il treno " + treno + " passa")
-	}
-
-	else{
-		console.log("il treno " + treno + " non passa")
-	}
-};
+for (treno; treno <= trenitoali; treno++) {
+    if (treno <= operativi) {
+        console.log("il treno " + treno + " passa")
+    }
+    else {
+        console.log("il treno " + treno + " non passa")
+    }
+}; */
 
 // Versione 3
+// var trenitoali = 12;
+//var operativi = 6;
+//var treno = 1;
+//var speciale = 10;
+//var giorno = 'sabato';
+//var oggi = 'sabato';
+//for (treno; treno <= trenitoali; treno++) {
+//if (treno <= operativi) {
+//    console.log("il treno " + treno + " passa")
+//   }
+//  else if (treno == speciale && oggi == giorno) {
+//      console.log("italo" + treno + " sta arrivando")
+// }
+//  else {
+//      console.log("treno" + treno + "non arriva")
+//  }
+//console.log("il treno " + treno + " non passa")
+// };
+//var testo = prompt("inserisci il tuo nome");
+//console.log(testo);
 
-var trenitoali = 12;
-var operativi = 6;
-var treno = 1;
-var speciale = 10;
-var giorno = 'sabato';
-var oggi = 'sabato';
+// Proposta 1
+var nome;
+while (nome !== "") {
+    nome = prompt("Chi sei?");
+  }
 
+//Proposta 2
 
-for (treno; treno <= trenitoali; treno++){
-
-	if (treno <= operativi){
-
-		console.log("il treno " + treno + " passa")
-	} else  if(treno == speciale && oggi == giorno) {
-
-		console.log("italo" + treno + " sta arrivando")
-
-	} else{
-			console.log("treno" + treno + "non arriva")
-		}
-
-		//console.log("il treno " + treno + " non passa")
-	};
-
-	var testo = prompt()
-
-	console.log(testo)
+/* do {
+    input = prompt("Give input");
+ } while (input == null || input == "");
+ console.log(input); */
+ dddd
